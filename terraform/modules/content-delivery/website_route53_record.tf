df6581b508c9691a53aec777c61f1882c727295e
vynnydev <vynnyprudencio@gmail.com>
2024-09-19 03:47:46 -0300

    feat: added change from load balancer dns to cloudfront dns to hosted and reach website

--- terraform/modules/content-delivery/website_route53_record.tf
+++ terraform/modules/content-delivery/website_route53_record.tf
@@ -4,12 +4,12 @@ resource "aws_route53_record" "www_record" {
   type    = "A"
   alias {
     # LOAD BALANCER
-    name                   = var.website_load_balancer_dns_name
-    zone_id                = var.website_lb_zone_id
+    #name                   = var.website_load_balancer_dns_name
+    # zone_id                = var.website_lb_zone_id
 
     # CLOUDFRONT
-    # name                   = aws_cloudfront_distribution.terrafarming_distribution.domain_name
-    # zone_id                = aws_cloudfront_distribution.terrafarming_distribution.hosted_zone_id
+    name                   = aws_cloudfront_distribution.website_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.website_distribution.hosted_zone_id
     evaluate_target_health = true
   }
 }
